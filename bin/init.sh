b6fd493f68a58bc4135b1914542245f2ad617586
Goran Mekić <meka@tilda.center>
2017-11-11 02:00:32 +0100

    Initialize consul

--- bin/init.sh
+++ bin/init.sh
@@ -1,17 +1,19 @@
 PROJECT_BIN_PATH=`dirname "${0}"`
-PROJECT_ROOT=`dirname "${PROJECT_BIN_PATH}"`
+PROJECT_ROOT=`readlink -f "${PROJECT_BIN_PATH}/.."`
 CBSD_WORKDIR="/cbsd"
-SSHD_FLAGS=`sysrc sshd_flags | cut -f 2- -d ':' | awk '{print $1}'`
-HOSTNAME="thinker.meka.no-ip.org"
-SET_HOSTNAME=""
+DOMAIN="my.domain"
+SSHD_FLAGS=`sysrc -n sshd_flags`
+SHORT_HOSTNAME=`hostname -s`
+HOSTNAME=`hostname`
+RESOLVER=`grep '^nameserver' /etc/resolv.conf | head -n 1 | awk '{print $2}'`
 
 
 if [ ! -d "${CBSD_WORKDIR}" ]; then
     zfs create -o mountpoint=${CBSD_WORKDIR} zroot${CBSD_WORKDIR}
 fi
 
-if [ `hostname` == `hostname -s` ]; then
-    SET_HOSTNAME="yes"
+if [ "${HOSTNAME}" == "${SHORT_HOSTNAME}" ]; then
+    HOSTNAME="${SHORT_HOSTNAME}.${DOMAIN}"
     hostname ${HOSTNAME}
     sysrc hostname="${HOSTNAME}"
 fi
@@ -20,6 +22,14 @@ if [ -z "${SSHD_FLAGS}" ]; then
     sysrc sshd_flags=""
 fi
 
-echo ${INIT_COMMAND}
-${INIT_COMMAND}
 env workdir="${CBSD_WORKDIR}" /usr/local/cbsd/sudoexec/initenv "${PROJECT_ROOT}/templates/initenv.conf"
+service cbsdd start
+service cbsdrsyncd start
+echo 1 | cbsd jcreate jconf="${PROJECT_ROOT}/templates/consul.conf"
+cat <<EOF >"${CBSD_WORKDIR}/jails-data/consul-data/etc/rc.conf.d/consul"
+consul_enable="YES"
+consul_args="-node=${SHORT_HOSTNAME} -bind=127.0.2.1 -client=127.0.2.1 -recursor=${RESOLVER} -ui -bootstrap -server"
+EOF
+mkdir "${CBSD_WORKDIR}/jails-data/consul-data/usr/local/etc/consul.d"
+echo 'sendmail_enable="NONE"' >"${CBSD_WORKDIR}/jails-data/consul-data/etc/rc.conf.d/sendmail"
+cbsd jstart consul
