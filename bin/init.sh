481dd17fa549f22c8bec729d19acf0dd8255149a
Goran Mekić <meka@tilda.center>
2017-11-11 02:48:39 +0100

    Setup interfaces

--- bin/init.sh
+++ bin/init.sh
@@ -6,6 +6,34 @@ SSHD_FLAGS=`sysrc -n sshd_flags`
 SHORT_HOSTNAME=`hostname -s`
 HOSTNAME=`hostname`
 RESOLVER=`grep '^nameserver' /etc/resolv.conf | head -n 1 | awk '{print $2}'`
+CLONED_INTERFACES=`sysrc -n cloned_interfaces`
+
+rm -rf /tmp/ifaces.txt
+touch /tmp/ifaces.txt
+for iface in ${CLONED_INTERFACES}; do
+    echo "${iface}" >>/tmp/ifaces.txt
+done
+
+
+LO1_INTERFACE=`grep '^lo1$' /tmp/ifaces.txt`
+if [ -z "${LO1_INTERFACE}" ]; then
+    if [ -z "${CLONED_INTERFACES}" ]; then
+        CLONED_INTERFACES="lo1"
+    else
+        CLONED_INTERFACES="${CLONED_INTERFACES} lo1"
+    fi
+    sysrc ifconfig_lo1="up"
+fi
+
+BRIDGE1_INTERFACE=`grep '^bridge1$' /tmp/ifaces.txt`
+if [ -z "${BRIDGE1_INTERFACE}" ]; then
+    CLONED_INTERFACES="${CLONED_INTERFACES} bridge1"
+    EGRESS=`netstat -rn | grep '^default' | awk '{print $4}'`
+    sysrc ifconfig_bridge1="inet 172.16.0.1 netmask 255.255.255.0 description ${EGRESS}"
+fi
+
+sysrc cloned_interfaces="${CLONED_INTERFACES}"
+service netif cloneup
 
 
 if [ ! -d "${CBSD_WORKDIR}" ]; then
