6cb8f8950ea9302a436bc0613301e157a710cc59
Goran Mekić <meka@tilda.center>
2018-03-15 14:41:20 +0100

    regenerate master.passwd

--- mk/service.mk
+++ mk/service.mk
@@ -41,13 +41,13 @@ up: setup
 	@sudo cbsd jexec jname=${SERVICE} pw groupadd devel -g ${GID}
 	@sudo cbsd jexec jname=${SERVICE} pw useradd devel -u ${UID} -g devel -s /bin/tcsh -G wheel,operator -m
 	@sudo cbsd jexec jname=${SERVICE} chpass -p '$6$MIv4IXAika7jqFH2$GkYSBax0G9CIBG0DcgQMP5gG7Qt.CojExDcU7YOQy0K.pouAd.edvo/MaQPhVO0fISxjOD4J1nzRsGVXUAxGp1' devel
-	@sudo cbsd jexec jname=${SERVICE} pwd_mkdb /etc/master.passwd
 .endif
 .else
 	@echo 'Deleting user "devel"'
 	-@sudo cbsd jexec jname=${SERVICE} pw user del devel -r >/dev/null 2>&1
 	@echo 'User "devel" deleted'
 .endif
+	@sudo cbsd jexec jname=${SERVICE} pwd_mkdb /etc/master.passwd
 .if !exists(.provisioned)
 	@${MAKE} ${MAKEFLAGS} provision
 .endif
