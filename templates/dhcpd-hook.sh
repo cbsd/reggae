ec9d1824756f494765a9056f94a1515fb8bffe29
Goran Mekić <meka@tilda.center>
2018-04-07 03:57:23 +0200

    Handle all DHCP events

--- templates/dhcpd-hook.sh
+++ templates/dhcpd-hook.sh
@@ -2,11 +2,13 @@
 
 ACTION=$1
 IP=$2
-MAC=$3
 
 if [ "${ACTION}" = "commit" ]; then
   /sbin/pfctl -t cbsd -T delete $IP || true
   /sbin/pfctl -t cbsd -T add $IP || true
 elif [ "${ACTION}" = "release" ]; then
+  /sbin/pfctl -t cbsd -T delete $IP || true
+  /sbin/pfctl -t cbsd -T add $IP || true
+elif [ "${ACTION}" = "expiry" ]; then
   /sbin/pfctl -t cbsd -T delete $IP || true
 fi
