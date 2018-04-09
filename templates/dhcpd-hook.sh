7c12d26e65a5d9d8872eaabd74c209524db0c978
Goran Mekić <meka@tilda.center>
2018-04-09 13:12:16 +0200

    Proper dhcpd hook

--- templates/dhcpd-hook.sh
+++ templates/dhcpd-hook.sh
@@ -4,11 +4,9 @@ ACTION=$1
 IP=$2
 
 if [ "${ACTION}" = "commit" ]; then
-  /sbin/pfctl -t cbsd -T delete $IP || true
   /sbin/pfctl -t cbsd -T add $IP || true
 elif [ "${ACTION}" = "release" ]; then
   /sbin/pfctl -t cbsd -T delete $IP || true
-  /sbin/pfctl -t cbsd -T add $IP || true
 elif [ "${ACTION}" = "expiry" ]; then
   /sbin/pfctl -t cbsd -T delete $IP || true
 fi
