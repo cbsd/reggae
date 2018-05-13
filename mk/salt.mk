82bf544abb172e31bd19aa31c11582a45041e686
Goran Mekić <meka@tilda.center>
2018-05-13 17:34:37 +0200

    Provision minion

--- mk/salt.mk
+++ mk/salt.mk
@@ -1,5 +1,11 @@
+MASTER = "salt.${DOMAIN}"
+
 do-provision:
 	@sudo cbsd jexec jname=${SERVICE} pkg install -y py36-salt
+	@sudo mkdir ${CBSD_WORKDIR}/jails-data/${SERVICE}-data/usr/local/etc/salt/minion.d >/dev/null 2>&1 || true
+	@sudo sh -c "echo master: ${MASTER} >${CBSD_WORKDIR}/jails-data/${SERVICE}-data/usr/local/etc/salt/minion.d/master.conf"
+	@sudo sh -c "echo salt_minion_enable=\"YES\" >${CBSD_WORKDIR}/jails-data/${SERVICE}-data//etc/rc.conf.d/salt_minion"
+	@sudo cbsd jexec jname=${SERVICE} service salt_minion restart
 
 do-clean:
 	@echo "Remove from master"
