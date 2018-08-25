f6e8e1ace3c3080aef838edd205f4e15d01737ad
Goran Mekić <meka@tilda.center>
2018-08-25 22:00:39 +0200

    Rename imported image

--- scripts/import.sh
+++ scripts/import.sh
@@ -2,7 +2,7 @@
 
 CBSD_WORKDIR=`sysrc -n cbsd_workdir`
 IMAGE_PATH="${1}"
-shift
+DOMAIN=`reggae get-config DOMAIN`
 
 if [ -z "${IMAGE_PATH}" ]; then
   echo "Usage: ${0} <image>" 2>&1
@@ -15,6 +15,8 @@ if [ ! -e "${IMAGE_PATH}" ]; then
 fi
 
 IMAGE=`basename ${IMAGE_PATH}`
+JAIL=`echo ${IMAGE} | sed 's;\.img$;;'`
 trap "rm -rf ${CBSD_WORKDIR}/import/${IMAGE}" HUP INT ABRT BUS TERM  EXIT
 cp "${IMAGE_PATH}" "${CBSD_WORKDIR}/import/${IMAGE}"
-cbsd jimport "${CBSD_WORKDIR}/import/${IMAGE}"
+cbsd jimport "${IMAGE}"
+cbsd jset jname=${JAIL} host_hostname=${JAIL}.${DOMAIN}
