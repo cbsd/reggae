d4e8e9599a73420117e3e7186d4e381f643e71b8
Goran Mekić <meka@tilda.center>
2019-04-27 20:35:17 +0200

    Remove record by IP

--- templates/reggae-register.sh
+++ templates/reggae-register.sh
@@ -2,24 +2,24 @@
 
 ACTION=$1
 IP=$2
-JAIL_NAME=$3
+NAME=$3
 ZONE_FILE="/var/unbound/conf.d/cbsd.zone"
-IGNORE_LINES=`/usr/bin/grep -n ORIGIN "${ZONE_FILE}" | /usr/bin/cut -f 1 -d ':'`
-EXISTING_DNS_ENTRY=`/usr/bin/sed -e "1,${IGNORE_LINES}d" "${ZONE_FILE}" | /usr/bin/grep "^${JAIL_NAME}"`
+IGNORE_LINES=`/usr/bin/grep -n ORIGIN "${ZONE_FILE}" | /usr/bin/head -n 1 | /usr/bin/cut -f 1 -d ':'`
+EXISTING_DNS_ENTRY=`/usr/bin/sed -e "1,${IGNORE_LINES}d" "${ZONE_FILE}" | /usr/bin/grep "^${NAME}"`
 
 
 if [ "${ACTION}" = "add" ]; then
-  /sbin/pfctl -t cbsd -T add $IP || true
-  if [ ! -z "${JAIL_NAME}" ]; then
+  /sbin/pfctl -t cbsd -T add $IP
+  if [ ! -z "${NAME}" ]; then
     if [ -z "${EXISTING_DNS_ENTRY}" ]; then
-      /bin/echo "${JAIL_NAME}    A   ${IP}" >>"${ZONE_FILE}"
+      /bin/echo "${NAME}    A   ${IP}" >>"${ZONE_FILE}"
     else
-      /usr/bin/sed -i "" "s/^${JAIL_NAME} *A .*/${JAIL_NAME}    A   ${IP}/" "${ZONE_FILE}"
+      /usr/bin/sed -i "" "s/^${NAME} *A .*/${NAME}    A   ${IP}/" "${ZONE_FILE}"
     fi
   fi
 elif [ "${ACTION}" = "delete" ]; then
-  /sbin/pfctl -t cbsd -T delete $IP || true
-  /usr/bin/sed -i "" "/^${JAIL_NAME} *A *.*/d" "${ZONE_FILE}"
+  /sbin/pfctl -t cbsd -T delete $IP
+  /usr/bin/sed -i "" "/^.* *A *${IP}$/d" "${ZONE_FILE}"
 fi
 
 local-unbound-control reload
