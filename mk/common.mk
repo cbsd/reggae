e1f4925f139fad7e5a100beddf204b260044f695
Goran Mekić <meka@tilda.center>
2019-09-09 13:09:41 +0200

    Write project based variables in project.mk

--- mk/common.mk
+++ mk/common.mk
@@ -1,3 +1,17 @@
 TYPE ?= jail
 EXTRA_FSTAB ?= templates/fstab
 XORG ?= NO
+DEVEL_MODE ?= NO
+RUNNING_UID != id -u
+RUNNING_GID != id -g
+UID ?= ${RUNNING_UID}
+GID ?= ${RUNNING_GID}
+DOMAIN != reggae get-config DOMAIN
+CBSD_WORKDIR != sysrc -n cbsd_workdir
+EXTRA_PACKAGES =
+
+.for provisioner in ${PROVISIONERS}
+.if ${provisioner} == "ansible"
+EXTRA_PACKAGES += python
+.endif
+.endfor
--- mk/project.mk
+++ mk/project.mk
@@ -51,10 +51,10 @@ fetch:
 setup:
 	@sudo rm -f services/*/cbsd.conf
 .for service url in ${SERVICES}
-	@rm -f services/${service}/vars.mk
-	@echo "DEVEL_MODE ?= ${DEVEL_MODE}" >>services/${service}/vars.mk
-	@echo "GID ?= ${GID}" >>services/${service}/vars.mk
-	@echo "UID ?= ${UID}" >>services/${service}/vars.mk
+	@rm -f services/${service}/project.mk
+	@echo "DEVEL_MODE ?= ${DEVEL_MODE}" >>services/${service}/project.mk
+	@echo "GID ?= ${GID}" >>services/${service}/project.mk
+	@echo "UID ?= ${UID}" >>services/${service}/project.mk
 .endfor
 .if target(post_setup)
 	@${MAKE} ${MAKEFLAGS} post_setup
