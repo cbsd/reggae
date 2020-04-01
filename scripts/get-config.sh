e4cd512fd998a875a18f2dd2739d67b4fd1deb1b
Goran Mekić <meka@tilda.center>
2020-04-01 10:34:46 +0200

    Configure pkg_proxy if defined

--- scripts/get-config.sh
+++ scripts/get-config.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
+SCRIPT_DIR=`dirname $0`
+. "${SCRIPT_DIR}/default.conf"
+
 if [ -f "/usr/local/etc/reggae.conf" ]; then
   . "/usr/local/etc/reggae.conf"
 fi
-
-SCRIPT_DIR=`dirname $0`
-. "${SCRIPT_DIR}/default.conf"
 
 raw_variable=`echo "$"$1`
 variable=`eval "echo" $raw_variable`
