b6ed0f221505e8c2b0a4a4341aa88a576b5b7b98
Goran Mekić <meka@tilda.center>
2020-09-06 12:20:33 +0200

    Firewall configuration improvements

--- mk/frameworks/freenit.service.mk
+++ mk/frameworks/freenit.service.mk
@@ -10,7 +10,13 @@ do_devel: init
 collect:
 	@rm -rf build
 	@mkdir -p build
-	@sudo cbsd jexec jname=${SERVICE} user=devel cmd=/usr/src/bin/collect.sh
+	@sudo cbsd jexec jname=${SERVICE} user=devel env OFFLINE=${offline} SYSPKG=${SYSPKG} /usr/src/bin/collect.sh
 
 do_publish:
 	@/bin/sh bin/publish.sh ${server} ${SERVICE}
+
+build_lib: up
+	@sudo cbsd jexec jname=${SERVICE} user=devel env OFFLINE=${offline} SYSPKG=${SYSPKG} /usr/src/bin/build.sh
+
+publish_lib: build_lib
+	@sudo cbsd jexec jname=${SERVICE} user=devel /usr/src/bin/publish.sh
