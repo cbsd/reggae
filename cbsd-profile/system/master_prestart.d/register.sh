83ed04256c5bd5602d32a494ac91c668d990656c
Goran Mekić <meka@tilda.center>
2022-01-29 22:57:37 +0100

    Fix path for NSD

--- cbsd-profile/system/master_prestart.d/register.sh
+++ cbsd-profile/system/master_prestart.d/register.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-lockf /var/unbound/conf.d/cbsd.zone.lock reggae register
+CBSD_WORKDIR=`sysrc -n cbsd_workdir`
+LOCK_DIR="${CBSD_WORKDIR}/jails-data/cbsd-data/usr/local/etc/nsd/zones/master"
+LOCK_FILE="${LOCK_DIR}/cbsd.zone.lock"
+
+if [ -d "${LOCK_DIR}" ]; then
+  lockf ${LOCK_FILE} reggae register
+else
+  reggae register
+fi
