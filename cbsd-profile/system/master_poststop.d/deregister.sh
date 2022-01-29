977ee82d5e447aad7be63f10c048634c0644af0a
Goran Mekić <meka@tilda.center>
2022-01-29 23:58:19 +0100

    Use /tmp for lock file

--- cbsd-profile/system/master_poststop.d/deregister.sh
+++ cbsd-profile/system/master_poststop.d/deregister.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 CBSD_WORKDIR=`sysrc -n cbsd_workdir`
-LOCK_DIR="${CBSD_WORKDIR}/jails-data/cbsd-data/usr/local/etc/nsd/zones/master"
+LOCK_DIR="${CBSD_WORKDIR}/tmp"
 LOCK_FILE="${LOCK_DIR}/cbsd.zone.lock"
 
 if [ -d "${LOCK_DIR}" ]; then
--- templates/dhcpd-hook.sh
+++ templates/dhcpd-hook.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-lockf /usr/local/etc/nsd/zones/master/cbsd.zone.lock /usr/local/bin/reggae-register.sh "$1" "$2" "$3" "$4"
+lockf /tmp/cbsd.zone.lock /usr/local/bin/reggae-register.sh "$1" "$2" "$3" "$4"
