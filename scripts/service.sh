47066dc897798a1890250b5048b117047103ab63
Goran Mekić <meka@tilda.center>
2022-07-07 12:47:15 +0200

    Write and use PIDs to kill children

--- scripts/service.sh
+++ scripts/service.sh
@@ -1,14 +1,25 @@
 #!/bin/sh
 
 SOCKET_DIR=${1}
-if [ -z "${SOCKET_DIR}" ]; then
-  echo "Usage: $0 <directory to put socket in>" >&2
+PID_FILE=${2}
+
+if [ -z "${SOCKET_DIR}" -o -z "${PID_FILE}" ]; then
+  echo "Usage: $0 <directory to put socket in> <pid file>" >&2
   exit 1
 fi
-SOCKET="${SOCKET_DIR}/reggae.sock"
-trap "rm -rf ${SOCKET}" HUP INT ABRT BUS TERM  EXIT
 
-rm -rf "${SOCKET}"
+SOCKET="${SOCKET_DIR}/reggae.sock"
+MYPID=""
+
+
+
+cleanup() {
+  pkill -P ${MYPID}
+  rm -rf ${SOCKET}
+}
+
+
+trap cleanup HUP INT ABRT BUS TERM  EXIT
 
 
 register_v4() {
@@ -31,19 +42,27 @@ deregister_v6() {
 }
 
 
-/usr/bin/nc -k -l -U "${SOCKET}" | while read action inet ip; do
-  echo "action = ${action}, inet = ${inet}, ip = ${ip}"
-  if [ "${action}" = "register" ]; then
-    if [ "${inet}" = "ipv4" ]; then
-      register_v4 ${ip}
-    elif [ "${inet}" = "ipv6" ]; then
-      register_v6 ${ip}
+run() {
+  rm -rf "${SOCKET}"
+  /usr/bin/nc -k -l -U "${SOCKET}" | while read action inet ip; do
+    echo "action = ${action}, inet = ${inet}, ip = ${ip}"
+    if [ "${action}" = "register" ]; then
+      if [ "${inet}" = "ipv4" ]; then
+        register_v4 ${ip}
+      elif [ "${inet}" = "ipv6" ]; then
+        register_v6 ${ip}
+      fi
+    elif [ "${action}" = "deregister" ]; then
+      if [ "${inet}" = "ipv4" ]; then
+        deregister_v4 ${ip}
+      elif [ "${inet}" = "ipv6" ]; then
+        deregister_v6 ${ip}
+      fi
     fi
-  elif [ "${action}" = "deregister" ]; then
-    if [ "${inet}" = "ipv4" ]; then
-      deregister_v4 ${ip}
-    elif [ "${inet}" = "ipv6" ]; then
-      deregister_v6 ${ip}
-    fi
-  fi
-done
+  done
+}
+
+run &
+MYPID=$!
+echo $$ >"${PID_FILE}"
+wait
