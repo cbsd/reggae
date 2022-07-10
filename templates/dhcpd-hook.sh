e0e8aaad11ee6443a28dd7f3059f28a36eda5d79
Goran Mekić <meka@tilda.center>
2022-07-10 02:13:56 +0200

    PTR zone for IPv6

--- templates/dhcpd-hook.sh
+++ templates/dhcpd-hook.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-lockf /tmp/cbsd.zone.lock /usr/local/bin/reggae-register.sh "$1" "$2" "$3" "$4"
+lockf /tmp/cbsd.zone.lock /usr/local/bin/reggae-register.sh "$1" "$2" "$3" "$4" "$5"
--- templates/dhcpd6-hook.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-lockf /tmp/cbsd.zone.lock /usr/local/bin/reggae-register6.sh "$1" "$2" "$3" "$4"
