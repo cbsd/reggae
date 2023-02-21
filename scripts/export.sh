117c2dd64f697cd7227f23f1b1da47f56ecb36a8
Goran Mekić <meka@tilda.center>
2023-02-21 08:07:17 +0100

    Add destination to export script

--- scripts/export.sh
+++ scripts/export.sh
@@ -4,11 +4,17 @@
 set -e
 
 SERVICE="${1}"
+DESTINATION="${2}"
+if [ -z "${SERVICE}" -o -z "${DESTINATION}" ]; then
+  echo "Usage: $0 <service> <destination>" >&2
+  exit 1
+fi
+
 BASE_WORKDIR="$(reggae get-config BASE_WORKDIR)"
 JAIL_PATH="${BASE_WORKDIR}/${SERVICE}"
 CONFIG="$(cat "/etc/jail.conf.d/${SERVICE}.conf")"
 
 
 service jail stop "${SERVICE}"
-tar --zstd -c -p -f "build/${SERVICE}.img" --cd "${JAIL_PATH}" .
-setextattr system config "${CONFIG}" "build/${SERVICE}.img"
+tar --zstd -c -p -f "${DESTINATION}/${SERVICE}.img" --cd "${JAIL_PATH}" .
+setextattr system config "${CONFIG}" "${DESTINATION}/${SERVICE}.img"
