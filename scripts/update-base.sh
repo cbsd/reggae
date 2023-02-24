f55694dfa5f4f6702aaa03f8a5daf2bb4eacfe86
Goran Mekić <meka@tilda.center>
2023-02-24 11:05:25 +0100

    Improve images and epair cleanup

--- scripts/update-base.sh
+++ scripts/update-base.sh
@@ -11,9 +11,33 @@ SCRIPT_DIR=$(dirname $0)
 if [ "${BACKEND}" = "cbsd" ]; then
   cbsd baseupdate
 elif [ "${BACKEND}" = "base" ]; then
-  cd "${BASE_WORKDIR}"
-  ls -1 | while read jail_name; do
-    env PAGER=cat "HTTP_PROXY=${PKG_PROXY}" freebsd-update -d "${BASE_WORKDIR}/${jail_name}" --not-running-from-cron --currently-running $(chroot $(jls -j "${jail_name}" path) freebsd-version -u) fetch install
-  done
-  cd -
+  JAIL="${1}"
+  export PAGER=cat
+  export HTTP_PROXY="${PKG_PROXY}"
+  if [ -z "${JAIL}" ]; then
+    cd "${BASE_WORKDIR}"
+    ls -1 | while read jail_name; do
+      if [ -x "${jail_name}/bin/freebsd-version" ]; then
+        echo "=== ${jail_name} ==="
+        export CURRENTLY_RUNNING="$(chroot "${BASE_WORKDIR}/${jail_name}" freebsd-version -u)"
+        freebsd-update \
+          -d "${BASE_WORKDIR}/${jail_name}" \
+          --not-running-from-cron \
+          --currently-running "${CURRENTLY_RUNNING}" \
+          fetch install
+        echo
+      fi
+    done
+    cd -
+  elif [ -x "${BASE_WORKDIR}/${JAIL}/bin/freebsd-version" ]; then
+    echo "=== ${JAIL} ==="
+    export CURRENTLY_RUNNING="$(chroot "${BASE_WORKDIR}/${JAIL}" freebsd-version -u)"
+    freebsd-update \
+      -d "${BASE_WORKDIR}/${JAIL}" \
+      --not-running-from-cron \
+      --currently-running "${CURRENTLY_RUNNING}" \
+      fetch install
+  else
+    echo "Something is wrong"
+  fi
 fi
