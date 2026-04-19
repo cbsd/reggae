61539178376f0715bcc36350839f629c4eabe184
Goran Mekić <meka@tilda.center>
2026-04-19 11:54:53 +0200

    Add stalwart service (#361)

--- mk/use.mk
+++ mk/use.mk
@@ -24,6 +24,7 @@ USE_USED_postfix ?= https://github.com/mekanix/jail-postfix
 USE_USED_prosody ?= https://github.com/mekanix/jail-prosody
 USE_USED_radicale ?= https://github.com/mekanix/jail-radicale
 USE_USED_rspamd ?= https://github.com/mekanix/jail-rspamd
+USE_USED_stalwart ?= https://github.com/mekanix/jail-stalwart
 USE_USED_turn ?= https://github.com/mekanix/jail-turn
 USE_USED_webmail ?= https://github.com/mekanix/jail-webmail
 USE_USED_wordpress ?= https://github.com/mekanix/jail-wordpress
